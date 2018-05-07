6d7a19874c3a75d75ecb3ec33faf84638949bb3b
chirusandeep <chirusandep@gmail.com>
2018-05-07 17:53:06 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NavbarFeatures from './NavbarFeatures.js';
+class App extends Component {
+  render() {
+    return (
+      
+     <NavbarFeatures />
+    );
+  }
+}
+
+export default App;
