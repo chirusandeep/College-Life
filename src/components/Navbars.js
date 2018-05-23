fc3a1e363bb79728ba8ec7b136a92d43c69ce102
radha98 <krishnaradha.t@gmail.com>
2018-05-23 11:13:46 +0530

    components added

---
+++ src/components/Navbars.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'mdbreact';
+
+
+export default class Navbars extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapse: false,
+            isWideEnough: false,
+            dropdownOpen: false
+        };
+    this.onClick = this.onClick.bind(this);
+    this.toggle = this.toggle.bind(this);
+    }
+
+    onClick(){
+        this.setState({
+            collapse: !this.state.collapse,
+        });
+    }
+
+    toggle() {
+        this.setState({
+            dropdownOpen: !this.state.dropdownOpen
+        });
+    }
+
+    render() {
+        return (
+
+                <Navbar color="unique-color-dark" dark expand="md" fixed="top" scrolling>
+                    <NavbarBrand href="/">
+                        <strong>College Life</strong>
+                    </NavbarBrand>
+                    { !this.state.isWideEnough && <NavbarToggler onClick = { this.onClick } />}
+                    <Collapse isOpen = { this.state.collapse } navbar>
+                        <NavbarNav left>
+                          <NavItem active>
+                              <NavLink to="#">Home</NavLink>
+                          </NavItem>
+                          <NavItem>
+                              <NavLink to="#">Notifcations</NavLink>
+                          </NavItem>
+                          <NavItem>
+                              <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+                              <DropdownToggle nav caret>Departments</DropdownToggle>
+                              <DropdownMenu>
+                                  <DropdownItem href="#">CSE</DropdownItem>
+                                  <DropdownItem href="#">ECE</DropdownItem>
+                                  <DropdownItem href="#">EEE</DropdownItem>
+                                  <DropdownItem href="#">MECH</DropdownItem>
+                                  <DropdownItem href="#">CIVIL</DropdownItem>
+                              </DropdownMenu>
+                              </Dropdown>
+                          </NavItem>
+                        </NavbarNav>
+                        <NavbarNav right>
+                          <NavItem>
+                            <form className="form-inline md-form mt-0">
+                              <input className="form-control mr-sm-2 mb-0 text-white" type="text" placeholder="Search" aria-label="Search"/>
+                            </form>
+                          </NavItem>
+                        </NavbarNav>
+                    </Collapse>
+                </Navbar>
+
+        );
+    }
+}
